a0ddc258e89fce1a511e4b45ef71969630ce896e
Miroslav Tzarev <mtzarev@gmail.com>
2024-03-22 19:56:40 +0200

    Add files via upload

---
+++ ArraysAdvancedSmallestNumbers.js
@@ -0,0 +1,8 @@
+function smallestNumbers(input) {
+    let sorted = input.sort((a,b)=>{
+        return a-b
+    });
+
+    console.log(sorted.slice(0,2).join(` `));
+}
+smallestNumbers([3, 0, 10, 4, 7, 3]);
