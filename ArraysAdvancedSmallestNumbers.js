5f121245a1d1e93659bf7942a3de6368e96000a2
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-10 18:32:55 +0300

    Update ArraysAdvancedSmallestNumbers.js

--- ArraysAdvancedSmallestNumbers.js
+++ ArraysAdvancedSmallestNumbers.js
@@ -2,7 +2,6 @@ function smallestNumbers(input) {
     let sorted = input.sort((a,b)=>{
         return a-b
     });
-
     console.log(sorted.slice(0,2).join(` `));
 }
 smallestNumbers([3, 0, 10, 4, 7, 3]);
